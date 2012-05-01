694afce3accb99b8511a2582a03e59079f207eae
Niklas Simonis <niklas.simonis@mni.thm.de>
2012-05-01 22:38:44 +0200

    THM Groups Model Edit Group new tests

--- tests/unit-tests/site/THMGroupsModelEditGroupTest.php
+++ tests/unit-tests/site/THMGroupsModelEditGroupTest.php
@@ -1,6 +1,6 @@
 <?php
 
-//require_once JPATH_BASE.'/components/com_thm_groups/models/advanced.php';
+require_once JPATH_BASE.'/components/com_thm_groups/models/editgroup.php';
 require_once 'PHPUnit.php';
 
 class THMGroupsModelEditGroupTest extends PHPUnit_TestCase
@@ -14,20 +14,50 @@ class THMGroupsModelEditGroupTest extends PHPUnit_TestCase
 
 	// PHPUnit_TestCase funtcion - overwritten
 	function setUp() {
-		//$this->instance = new THMGroupsModelAdvanced();
+		$this->instance = new THMGroupsModelEditGroup();
+		// GSGID get ID from Group Registered
+		$array['gsgid'] = '2';
+		$array['gid'] = '1';
+		JRequest::set($array, 'post');
 	}
 
 	// Kill instance
 	function tearDown() {
-		//unset($this->instance);
+		unset($this->instance);
 	}
 	
-	function testgetViewParams(){
-		//$result = $this->instance->isComponentAvailable($comp);
-		$result = true;
+	function testdelPic(){
+		$gid = "1";
+		$db =& JFactory::getDBO();
+		
+		$result = $this->instance->delPic();
 		$expected = true;
-		$this->assertTrue($result == $expected);
+		
+		$query = "UPDATE #__thm_groups_groups SET picture=NULL WHERE id = $gid ";
+		$db->setQuery( $query );
+	
+		$db->query();
+		$this->assertEquals($result,$expected);
 	}
+	
+	function testgetParentIdPublic(){
+		
+		$result = $this->instance->getParentId();
+		$expected = "1"; // ID from Group Public
+		
+		$this->assertEquals($expected, $result);
+	}
+	
+	function testgetAllGroups(){
+		$result = $this->instance->getAllGroups();
+	
+		$this->assertEquals($result[0]->id,"1");
+		$this->assertEquals($result[0]->parent_id,"0");
+		$this->assertEquals($result[0]->lft,"1");
+		$this->assertEquals($result[0]->rgt,"124");
+		$this->assertEquals($result[0]->title,"Public");
+	}
+	
 }
 
 ?>
--- tests/unit-tests/testsuite.php
+++ tests/unit-tests/testsuite.php
@@ -4,7 +4,7 @@ require_once 'framework_include.php';
 
 require_once 'site/THMGroupsModelAdvancedTest.php';
 //require_once 'THMGroupsModeleditTest.php';
-//require_once 'THMGroupsModelEditGroupTest.php';
+require_once 'site/THMGroupsModelEditGroupTest.php';
 //require_once 'THMGroupsModelGroupsTest.php';
 //require_once 'THMGroupsModelListTest.php';
 //require_once 'THMGroupsModelProfileTest.php';
@@ -14,7 +14,7 @@ $suite = new PHPUnit_TestSuite("Component THM Groups Site Model Test");
 echo  $suite->getName() . "\n";
 $suite->addTestSuite("THMGroupsModelAdvancedTest");
 //$suite->addTestSuite("THMGroupsModeleditTest");
-//$suite->addTestSuite("THMGroupsModelEditGroupTest");
+$suite->addTestSuite("THMGroupsModelEditGroupTest");
 //$suite->addTestSuite("THMGroupsModelGroupsTest");
 //$suite->addTestSuite("THMGroupsModelListTest");
 //$suite->addTestSuite("THMGroupsModelProfileTest");
