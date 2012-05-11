21be40d166cad7507eb3acdf22608305e424a7c7
Niklas Simonis <niklas.simonis@mni.thm.de>
2012-05-11 15:11:53 +0200

    test added

--- tests/unit-tests/admin/THMGroupsModelEditRoleAdminTest.php
+++ tests/unit-tests/admin/THMGroupsModelEditRoleAdminTest.php
@@ -22,8 +22,41 @@ class THMGroupsModelEditRoleAdminTest extends PHPUnit_TestCase
 		unset($this->instance);
 	}
 	
-	function test(){
+	/*
+	 * No tests:
+	* getData
+	*/
 	
+	// tests _buildQuery()
+	// function returns an SQL query
+	// should start with SELECT
+	function test_buildQuery(){
+		$result = $this->instance->_buildQuery();
+		$expected = "SELECT ";
+		$this->assertContains($expected, $result);
+	}
+	
+	// tests store
+	// function updates some values in database
+	// should return true
+	function teststore(){
+		$array['role_name'] = 'THMGroupsSuiteTest';
+		$array['rid'] = '999999';
+		JRequest::set($array, 'post');
+		
+		$db =& JFactory::getDBO();
+		$query = "INSERT INTO #__thm_groups_roles (`id`, `name`)";
+		$query .= "VALUES ('999999' ,  'THMGroupsSuite')";
+		$db->setQuery( $query );
+		$db->query();
+		
+		$result = $this->instance->store();
+		
+		$query = "DELETE FROM #__thm_groups_roles WHERE id = '999999'";
+		$db->setQuery($query);
+		$db->query();
+		
+		$this->assertTrue($result);
 	}
 }
 
