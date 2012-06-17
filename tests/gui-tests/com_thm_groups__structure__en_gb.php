f86899fa9adbbc857a823bb70b140dd71fe115e8
Mariusz Homeniuk <mariusz.homeniuk@mni.thm.de>
2012-06-17 11:53:30 +0200

    gui test structure fix

--- tests/gui-tests/com_thm_groups__structure__en_gb.php
+++ tests/gui-tests/com_thm_groups__structure__en_gb.php
@@ -9,14 +9,12 @@ class ComThmGroupsStructureEnGb extends PHPUnit_Extensions_SeleniumTestCase
 
   public function testMyTestCase()
   {
-    $this->open("/joomla/administrator/index.php");
+    $this->open("/joomla25/administrator/index.php");
     $this->type("id=mod-login-username", "admin");
     $this->type("id=mod-login-password", "adminadmin");
     $this->select("id=lang", "value=en-GB");
-    $this->click("link=Log in");
-    $this->waitForPageToLoad("30000");
-    $this->click("link=Structure");
-    $this->waitForPageToLoad("30000");
+    $this->clickAndWait("link=Log in");
+    $this->clickAndWait("link=Structure");
     $this->assertTrue($this->isElementPresent("link=Add Entry"));
     $this->assertTrue($this->isElementPresent("css=span.icon-32-moderate"));
     $this->assertTrue($this->isElementPresent("link=Edit Entry"));
@@ -33,30 +31,25 @@ class ComThmGroupsStructureEnGb extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertTrue($this->isTextPresent("EMail"));
     $this->assertTrue($this->isTextPresent("Titel"));
     $this->assertTrue($this->isTextPresent("Mode"));
-    $this->click("link=Add Entry");
-    $this->waitForPageToLoad("30000");
+    $this->clickAndWait("link=Add Entry");
     $this->assertTrue($this->isElementPresent("id=name"));
     $this->type("id=name", "Testfield");
     $this->assertTrue($this->isElementPresent("id=relation"));
     $this->select("id=relation", "label=text");
-    $this->assertTrue($this->isElementPresent("id=TEXT_extra"));
+    $this->waitForCondition("selenium.browserbot.getCurrentWindow().document.getElementById('TEXT_extra')");
     $this->type("id=TEXT_extra", "50");
-    $this->click("link=Save & Close");
-    $this->waitForPageToLoad("30000");
+    $this->clickAndWait("link=Save & Close");
     $this->assertTrue($this->isTextPresent("Data Saved!"));
     $this->assertTrue($this->isElementPresent("link=Testfield"));
-    $this->click("link=Testfield");
-    $this->waitForPageToLoad("30000");
+    $this->clickAndWait("link=Testfield");
     $this->type("id=name", "Testfield_test");
-    $this->click("link=Save & Close");
-    $this->waitForPageToLoad("30000");
+    $this->clickAndWait("link=Save & Close");
     $this->assertTrue($this->isTextPresent("Data Saved!"));
     $this->click("id=cb6");
     $this->click("link=Delete");
-    $this->assertEquals("COM_THM_GROUPS_REALLY_DELETE", $this->getConfirmation());
-    $this->assertFalse($this->isElementPresent("link=Testfield_test"));
-    $this->click("link=Log out");
-    $this->waitForPageToLoad("30000");
+    $this->assertEquals("COM_THM_GROUPS_REALLY_DELETE", $this->getConfirmation());	sleep(1);
+    $this->assertFalse($this->isElementPresent("link=Testfield_test"));				sleep(1);
+    $this->clickAndWait("link=Log out");
   }
 }
 ?>
