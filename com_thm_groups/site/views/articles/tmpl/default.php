f8d79c3e215f17e2eb952ff25d7f50d7fb3bef20
Dennis Priefer <dennis.priefer@mni.thm.de>
2012-09-10 11:22:28 +0200

    fixed code against coding standard

--- com_thm_groups/site/views/articles/tmpl/default.php
+++ com_thm_groups/site/views/articles/tmpl/default.php
@@ -46,18 +46,34 @@ $canCreate = $this->hasUserRightToCreateArticle($currCategoryID);
 		<div class="qp_filter_select">
 			<label class="qp_filter_label" for="filter_category_id"><?php echo JText::_('COM_THM_QUICKPAGES_CATEGORY_LABEL'); ?></label>
 			<select name="filter_category_id" class="qp_inputbox" onchange="this.form.submit()">
-				<?php /* echo JHtml::_('select.options', JHtml::_('category.options', 'com_content'), 'value', 'text', $this->state->get('filter.category_id')); */?>
+				<?php /* echo JHtml::_('select.options', 
+					JHtml::_('category.options', 'com_content'), 
+					'value', 
+					'text',
+					$this->state->get('filter.category_id')
+					); 
+				*/?>
 				<?php echo JHtml::_('select.options', $this->categories, 'id', 'title', $this->state->get('filter.category_id'));?>
 			</select>
 
 			<select name="filter_published" class="qp_inputbox" onchange="this.form.submit()">
 				<option value=""><?php echo JText::_('COM_THM_QUICKPAGES_SELECT_PUBLISHED');?></option>
-				<?php echo JHtml::_('select.options', JHtml::_('jgrid.publishedOptions'), 'value', 'text', $this->state->get('filter.published'), true);?>
+				<?php echo JHtml::_(
+						'select.options', 
+						JHtml::_('jgrid.publishedOptions'), 
+						'value', 
+						'text', 
+						$this->state->get('filter.published'), true
+						);?>
 			</select>
 		</div>
 		<div class="qp_filter_search">
 			<label class="qp_filter_label" for="filter_search"><?php echo JText::_('COM_THM_QUICKPAGES_FILTER_LABEL'); ?></label>
-			<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo JText::_('COM_THM_QUICKPAGES_FILTER_DESC'); ?>" />
+			<input type="text" name="filter_search" id="filter_search" value="<?php 
+				echo $this->escape($this->state->get('filter.search')); 
+			?>" title="<?php 
+			echo JText::_('COM_THM_QUICKPAGES_FILTER_DESC'); 
+			?>" />
 
 			<button type="submit" class="qp_button"><?php echo JText::_('COM_THM_QUICKPAGES_FILTER_SUBMIT'); ?></button>
 		</div>
@@ -74,10 +90,12 @@ $canCreate = $this->hasUserRightToCreateArticle($currCategoryID);
 					<?php echo JHtml::_('grid.sort', 'COM_THM_QUICKPAGES_PUBLISHED', 'a.state', $listDirn, $listOrder); ?>
 				</th>
 				<th width="18%">
-					<?php echo trim(JHtml::_('grid.sort',  'COM_THM_QUICKPAGES_ORDERING', 'a.ordering', $listDirn, $listOrder)); ?>
-					<?php if ($saveOrder) : ?>
-						<?php echo JHtml::_('grid.order',  $this->items, 'filesave.png', 'articles.saveorder'); ?>
-					<?php endif; ?>
+					<?php echo trim(JHtml::_('grid.sort',  'COM_THM_QUICKPAGES_ORDERING', 'a.ordering', $listDirn, $listOrder));
+					if ($saveOrder)
+					{
+						echo JHtml::_('grid.order',  $this->items, 'filesave.png', 'articles.saveorder');
+					}
+					?>
 				</th>
 				<th width="10%">
 					<?php echo JHtml::_('grid.sort', 'COM_THM_QUICKPAGES_DATE', 'a.created', $listDirn, $listOrder); ?>
@@ -91,12 +109,19 @@ $canCreate = $this->hasUserRightToCreateArticle($currCategoryID);
 					<?php
 						if ($canCreate AND $currCategoryID != 0)
 						{
-							/* $editURL = JRoute::_('index.php?option=com_content&task=article.add&catid='.$currCategoryID.$itemParam.$staticParams); */
-							$editURL = JRoute::_('index.php?option=com_content&view=form&layout=edit&catid=' . $currCategoryID . $itemParam . $staticParams);
+							/*$editURL = JRoute::_('index.php?option=com_content&task=article.add&catid='.$currCategoryID.$itemParam.$staticParams);*/
+							$editURL = JRoute::_('index.php?option=com_content&view=form&layout=edit&catid=' 
+									. $currCategoryID 
+									. $itemParam 
+									. $staticParams
+									);
 
 							$imgSpanTag = '<span class="qp_icon_big qp_create_icon"><span class="qp_invisible_text">New</span></span>';
 
-							echo JHTML::_('link', $editURL, $imgSpanTag, 'title="' . JText::_('COM_THM_QUICKPAGES_HTML_CREATE') . '" class="qp_icon_link"');
+							echo JHTML::_('link', $editURL, $imgSpanTag, 'title="' 
+									. JText::_('COM_THM_QUICKPAGES_HTML_CREATE') 
+									. '" class="qp_icon_link"'
+									);
 						}
 						else
 						{
@@ -117,7 +142,8 @@ $canCreate = $this->hasUserRightToCreateArticle($currCategoryID);
 		</tfoot>
 		<tbody>
 		-->
-		<?php foreach ($this->items as $i => $item) :
+		<?php foreach ($this->items as $i => $item)
+		{
 			$item->max_ordering = 0;
 			$ordering	= ($listOrder == 'a.ordering');
 
@@ -141,24 +167,85 @@ $canCreate = $this->hasUserRightToCreateArticle($currCategoryID);
 					?>
 				</td>
 				<td class="center">
-					<?php echo JHtml::_('jgrid.published', $item->state, $i, 'articles.', $canChange, 'cb', $item->publish_up, $item->publish_down); ?>
+					<?php 
+						echo JHtml::_('jgrid.published', $item->state, $i, 'articles.', $canChange, 'cb', $item->publish_up, $item->publish_down);
+					?>
 				</td>
 				<td class="order">
-					<?php if ($canChange) : ?>
-						<?php if ($saveOrder) : ?>
-							<?php if ($listDirn == 'asc') : ?>
-								<span><?php echo $this->pagination->orderUpIcon($i, ($item->catid == @$this->items[$i - 1]->catid), 'articles.orderup', 'JLIB_HTML_MOVE_UP', $ordering); ?></span>
-								<span><?php echo $this->pagination->orderDownIcon($i, $this->pagination->total, ($item->catid == @$this->items[$i + 1]->catid), 'articles.orderdown', 'JLIB_HTML_MOVE_DOWN', $ordering); ?></span>
-							<?php elseif ($listDirn == 'desc') : ?>
-								<span><?php echo $this->pagination->orderUpIcon($i, ($item->catid == @$this->items[$i - 1]->catid), 'articles.orderdown', 'JLIB_HTML_MOVE_UP', $ordering); ?></span>
-								<span><?php echo $this->pagination->orderDownIcon($i, $this->pagination->total, ($item->catid == @$this->items[$i + 1]->catid), 'articles.orderup', 'JLIB_HTML_MOVE_DOWN', $ordering); ?></span>
-							<?php endif; ?>
-						<?php endif; ?>
+					<?php if ($canChange)
+						  {
+						  	if ($saveOrder)
+						  	{
+						  		if ($listDirn == 'asc')
+						  		{ 
+						  	?>
+								<span>
+								<?php 
+									echo $this->pagination->orderUpIcon(
+											$i, 
+											($item->catid == @$this->items[$i - 1]->catid), 
+											'articles.orderup', 
+											'JLIB_HTML_MOVE_UP', 
+											$ordering
+											); 
+								?>
+								</span>
+								<span>
+								<?php 
+									echo $this->pagination->orderDownIcon(
+											$i, 
+											$this->pagination->total, 
+											($item->catid == @$this->items[$i + 1]->catid), 
+											'articles.orderdown', 
+											'JLIB_HTML_MOVE_DOWN', 
+											$ordering
+											);
+								?>
+								</span>
+							<?php   
+						  		}
+								elseif ($listDirn == 'desc') 
+								{
+							?>
+								<span>
+								<?php 
+									echo $this->pagination->orderUpIcon(
+											$i, 
+											($item->catid == @$this->items[$i - 1]->catid), 
+											'articles.orderdown', 
+											'JLIB_HTML_MOVE_UP', 
+											$ordering
+											);
+								?>
+								</span>
+								<span>
+								<?php 
+									echo $this->pagination->orderDownIcon(
+											$i, 
+											$this->pagination->total, 
+											($item->catid == @$this->items[$i + 1]->catid), 
+											'articles.orderup', 
+											'JLIB_HTML_MOVE_DOWN', 
+											$ordering
+											);
+								?>
+								</span>
+							<?php 
+								}
+							?>
+						<?php
+						  	} 
+						?>
 						<?php $disabled = $saveOrder ?  '' : 'disabled="disabled"'; ?>
-						<input type="text" name="order[]" size="5" value="<?php echo $item->ordering;?>" <?php echo $disabled ?> class="text-area-order" />
-					<?php else : ?>
-						<?php echo $item->ordering; ?>
-					<?php endif; ?>
+						<input type="text" name="order[]" size="5" value="<?php echo $item->ordering;?>" <?php 
+						echo $disabled ?> class="text-area-order" />
+					<?php 
+}
+						  else 
+						  { 
+							echo $item->ordering;
+						  } 
+				    ?>
 				</td>
 				<td class="center nowrap">
 					<?php echo JHTML::_('date', $item->created, JText::_('DATE_FORMAT_LC4')); ?>
@@ -184,7 +271,10 @@ $canCreate = $this->hasUserRightToCreateArticle($currCategoryID);
 							/* $editURL = JRoute::_('index.php?option=com_content&view=form&layout=edit&a_id='.$item->id.$itemParam.$staticParams); */
 							$imgSpanTag = '<span class="state edit"><span class="text">Edit</span></span>';
 
-							echo JHTML::_('link', $editURL, $imgSpanTag, 'title="' . JText::_('COM_THM_QUICKPAGES_HTML_EDIT_ITEM') . '" class="jgrid"');
+							echo JHTML::_('link', $editURL, $imgSpanTag, 'title="' 
+									. JText::_('COM_THM_QUICKPAGES_HTML_EDIT_ITEM') 
+									. '" class="jgrid"'
+									);
 							echo "\n";
 						}
 						else
@@ -198,8 +288,24 @@ $canCreate = $this->hasUserRightToCreateArticle($currCategoryID);
 							// Define state changes needed by JHtmlJGrid.state(), see also JHtmlJGrid.published()
 							$states	= array(
 								0	=> array(),		// Dummy: Wird nicht gebraucht, erzeugt aber sonst Notice
-								3	=> array('trash',	'JPUBLISHED',	'COM_THM_QUICKPAGES_HTML_TRASH_ITEM',	'JPUBLISHED',	false,	'trash',	'trash_disabled'),
-								-3	=> array('publish',	'JTRASHED',		'COM_THM_QUICKPAGES_HTML_UNTRASH_ITEM',	'JTRASHED',		false,	'untrash',	'untrash'),
+								3	=> array(
+										'trash',
+										'JPUBLISHED',
+										'COM_THM_QUICKPAGES_HTML_TRASH_ITEM',
+										'JPUBLISHED',
+										false,
+										'trash',
+										'trash_disabled'
+										),
+								-3	=> array(
+										'publish',
+										'JTRASHED',
+										'COM_THM_QUICKPAGES_HTML_UNTRASH_ITEM',
+										'JTRASHED',
+										false,
+										'untrash',
+										'untrash'
+										),
 							);
 
 							echo JHtml::_('jgrid.state', $states, ($item->state < 0 ? -3 : 3), $i, 'articles.', $canDelete);
@@ -207,7 +313,9 @@ $canCreate = $this->hasUserRightToCreateArticle($currCategoryID);
 					?>
 				</td>
 			</tr>
-			<?php endforeach; ?>
+<?php 
+}
+?>
 		</tbody>
 	</table>
 
