c4a80c744aa0393695633ced8cf66075e017e4ee
Dennis Priefer <dennis.priefer@mni.thm.de>
2012-09-14 10:45:58 +0200

    small fixes

--- com_thm_groups/all.sql
+++ com_thm_groups/all.sql
@@ -30,3 +30,16 @@ CREATE TABLE IF NOT EXISTS `jos_thm_groups_picture_extra` (
   `value` text NOT NULL,
   PRIMARY KEY (`structid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `#__thm_quickpages_map`
+--
+
+CREATE TABLE IF NOT EXISTS `#__thm_quickpages_map` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_kind` char(1) NOT NULL,
+  `catid` int(11) NOT NULL,
+   PRIMARY KEY  (`id`, `id_kind`, `catid`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
