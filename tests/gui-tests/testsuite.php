4073baefdb74b9060a7fb7fc1f3d22a801e735c2
Mariusz Homeniuk <mariusz.homeniuk@mni.thm.de>
2012-10-01 15:48:33 +0200

    .

--- tests/gui-tests/testsuite.php
+++ tests/gui-tests/testsuite.php
@@ -10,12 +10,12 @@ class AllComThmGroupsGuiTests
 		$suite = new PHPUnit_Framework_TestSuite('Component THM Groups GUI Test');
 
 		$suite->addTestFile(__DIR__.'/com_thm_groups__administration_home__en_gb.php');
-		$suite->addTestFile(__DIR__.'/com_thm_groups__groupmanager__en_gb.php');
+		//$suite->addTestFile(__DIR__.'/com_thm_groups__groupmanager__en_gb.php');
 		$suite->addTestFile(__DIR__.'/com_thm_groups__home__en_gb.php');
 		$suite->addTestFile(__DIR__.'/com_thm_groups__membermanager_1__en_gb.php');
 		//$suite->addTestFile(__DIR__.'/com_thm_groups__membermanager_2__en_gb.php');
-		$suite->addTestFile(__DIR__.'/com_thm_groups__rolemanager__en_gb.php');
-		$suite->addTestFile(__DIR__.'/com_thm_groups__structure__en_gb.php');
+		//$suite->addTestFile(__DIR__.'/com_thm_groups__rolemanager__en_gb.php');
+		//$suite->addTestFile(__DIR__.'/com_thm_groups__structure__en_gb.php');
 
 		return $suite;
 	}
