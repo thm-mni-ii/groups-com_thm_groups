952fe07eba8bd1373fb489917300a90b6b68a6e8
Mariusz Homeniuk <mariusz.homeniuk@mni.thm.de>
2012-10-22 14:31:33 +0200

    joomla gui tests aufgespaltet

--- tests/gui-tests/testsuite.php
+++ tests/gui-tests/testsuite.php
@@ -29,8 +29,8 @@ class AllComThmGroupsGuiTests
 
 		$suite->addTestFile(__DIR__.'/com_thm_groups__administration_home__en_gb.php');
 		//$suite->addTestFile(__DIR__.'/com_thm_groups__groupmanager__en_gb.php');
-		$suite->addTestFile(__DIR__.'/com_thm_groups__home__en_gb.php');
-		$suite->addTestFile(__DIR__.'/com_thm_groups__membermanager_1__en_gb.php');
+		//$suite->addTestFile(__DIR__.'/com_thm_groups__home__en_gb.php');
+		//$suite->addTestFile(__DIR__.'/com_thm_groups__membermanager_1__en_gb.php');
 		//$suite->addTestFile(__DIR__.'/com_thm_groups__membermanager_2__en_gb.php');
 		//$suite->addTestFile(__DIR__.'/com_thm_groups__rolemanager__en_gb.php');
 		//$suite->addTestFile(__DIR__.'/com_thm_groups__structure__en_gb.php');
