1189b6d0ea67b13759311c92201ff0c11da982cc
Ilja <ilja.michajlow@mni.thm.de>
2014-04-27 15:30:08 +0200

    JView to JViewLegacy

--- com_thm_groups/admin/views/addgroup/view.html.php
+++ com_thm_groups/admin/views/addgroup/view.html.php
@@ -28,7 +28,7 @@ jimport('joomla.filesystem.path');
  * @link      www.mni.thm.de
  * @since     Class available since Release 2.0
  */
-class THMGroupsViewAddGroup extends JView
+class THMGroupsViewAddGroup extends JViewLegacy
 {
     /**
      * Method to get display
