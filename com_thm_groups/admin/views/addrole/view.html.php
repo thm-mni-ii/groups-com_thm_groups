8926be13d0166729ed31bae4b2e1b77ac88a5972
Ilja <ilja.michajlow@mni.thm.de>
2014-04-27 15:30:54 +0200

    JView to JViewLegacy

--- com_thm_groups/admin/views/addrole/view.html.php
+++ com_thm_groups/admin/views/addrole/view.html.php
@@ -24,7 +24,7 @@ jimport('joomla.filesystem.path');
  * @link      www.mni.thm.de
  * @since     Class available since Release 2.0
  */
-class THMGroupsViewAddRole extends JView
+class THMGroupsViewAddRole extends JViewLegacy
 {
     /**
      * Method to get display
