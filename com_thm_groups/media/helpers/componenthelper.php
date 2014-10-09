de67a9d235845bc2032d1dbebfb47995b84dc8b6
Ilja <ilja.michajlow@mni.thm.de>
2014-10-09 13:59:56 +0200

    media added

---
+++ com_thm_groups/media/helpers/componenthelper.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @category    Joomla component
+ * @package     THM_Groups
+ * @subpackage  com_thm_groups.admin
+ * @name        provides functions useful to multiple component files
+ * @author      James Antrim, <james.antrim@mni.thm.de>
+ * @author      Wolf Rost, <wolf.rost@mni.thm.de>
+ * @copyright   2012 TH Mittelhessen
+ * @license     GNU GPL v.2
+ * @link        www.mni.thm.de
+ */
+
+/**
+ * Class providing functions usefull to multiple component files
+ *
+ * @category  Joomla.Component.Admin
+ * @package   thm_organizer
+ */
+class THM_ComponentHelper
+{
+    /**
+     * Configure the Linkbar.
+     *
+     * @param   object  &$view  the view context calling the function
+     *
+     * @return void
+     */
+    public static function addSubmenu(&$view)
+    {
+        $viewName = $view->get('name');
+
+        // No submenu creation while editing a resource
+        if (!strpos($viewName, 'manager') AND $viewName == 'thm_groups')
+        {
+            return;
+        }
+
+        JSubMenuHelper::addEntry(
+            JText::_('COM_THM_GROUPS_HOME'),
+            'index.php?option=com_thm_groups&view=thmgroups',
+            $viewName == 'thmgroups'
+        );
+
+        JSubMenuHelper::addEntry(
+            JText::_('COM_THM_GROUPS_MEMBERMANAGER'),
+            'index.php?option=com_thm_groups&view=membermanager',
+            $viewName == 'membermanager'
+        );
+        JSubMenuHelper::addEntry(
+            JText::_('COM_THM_GROUPS_GROUPMANAGER'),
+            'index.php?option=com_users&view=groups',
+            $viewName == 'groupmanager'
+        );
+        JSubMenuHelper::addEntry(
+            JText::_('COM_THM_GROUPS_ROLEMANAGER'),
+            'index.php?option=com_thm_groups&view=rolemanager',
+            $viewName == 'rolemanager'
+        );
+        JSubMenuHelper::addEntry(
+            JText::_('COM_THM_GROUPS_PROFILEMANAGER'),
+            'index.php?option=com_thm_groups&view=profilemanager',
+            $viewName == 'profilemanager'
+        );
+        JSubMenuHelper::addEntry(
+            JText::_('COM_THM_GROUPS_STRUCTUREMANAGER'),
+            'index.php?option=com_thm_groups&view=structuremanager',
+            $viewName == 'structuremanager'
+        );
+        JSubMenuHelper::addEntry(
+            JText::_('COM_THM_GROUPS_STRUCTURETYPEMANAGER'),
+            'index.php?option=com_thm_groups&view=structure_item_manager',
+            $viewName == 'structure_item_manager'
+        );
+        JSubMenuHelper::addEntry(
+            JText::_('COM_THM_GROUPS_DYNAMICTYPEMANAGER'),
+            'index.php?option=com_thm_groups&view=dynamic_type_manager',
+            $viewName == 'dynamic_type_manager'
+        );
+        JSubMenuHelper::addEntry(
+            JText::_('COM_THM_GROUPS_STATICTYPESMANAGER'),
+            'index.php?option=com_thm_groups&view=static_type_manager',
+            $viewName == 'static_types_manager'
+        );
+
+        $view->sidebar = JHtmlSidebar::render();
+    }
+}
