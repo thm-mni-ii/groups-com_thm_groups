bbd53acf6a6991c88e33d016a9803e62c06556b8
Ilja <ilja.michajlow@mni.thm.de>
2015-02-14 14:10:28 +0100

    Class for getting roles via ajax
    Change-Id: I76282cd54c565fe6ebcccb6ce63802e24807af31

---
+++ com_thm_groups/admin/models/roles_ajax.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @category    Joomla component
+ * @package     THM_Groups
+ * @subpackage  com_thm_groups.admin
+ * @name        THM_GroupsModelRoles
+ * @author      Ilja Michajlow, <ilja.michajlow@mni.thm.de>
+ * @copyright   2015 TH Mittelhessen
+ * @license     GNU GPL v.2
+ * @link        www.mni.thm.de
+ */
+defined('_JEXEC') or die;
+jimport('joomla.application.component.model');
+
+/**
+ * Class provides methods for building a model of the roles in JSON format
+ *
+ * @category    Joomla.Component.Site
+ * @package     thm_groups
+ * @subpackage  com_thm_groups.admin
+ */
+class THM_GroupsModelRoles_Ajax extends JModelLegacy
+{
+    /**
+     * Returns all roles of specified group
+     *
+     * @return json string
+     *
+     * @throws Exception
+     */
+    public function getRolesOfGroup()
+    {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $jinput = JFactory::getApplication()->input;
+
+        $groupID = $jinput->get->get('batch-groups', '', 'int');
+
+        $nestedQuery = $db->getQuery(true);
+        $nestedQuery
+            ->select('id')
+            ->from('#__thm_groups_users');
+
+
+        $query
+            ->select('r.id, r.name')
+            ->from('#__thm_groups_roles AS r')
+            ->innerJoin('#__thm_groups_usergroups_roles AS a ON r.id = a.rolesID')
+            ->innerJoin('#__thm_groups_users_usergroups_roles AS b ON a.id = b.usergroups_rolesID')
+            ->where('b.usersID IN (' . $nestedQuery .')')
+            ->group('r.id')
+            ->order('r.name ASC');
+
+        if(!empty($groupID))
+        {
+            $query->where("a.usergroupsID = $groupID");
+        }
+
+        $db->setQuery($query);
+        $db->execute();
+
+        $roles = $db->loadObjectList();
+
+        $result = array();
+        foreach($roles as $role)
+        {
+            $result[(int)$role->id] = $role->name;
+        }
+
+        $result = json_encode($result);
+
+        return $result;
+    }
+}
---
+++ com_thm_groups/admin/views/roles_ajax/view.raw.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @category    Joomla component
+ * @package     THM_Groups
+ * @subpackage  com_thm_groups.admin
+ * @name        THM_GroupsViewRoles_Ajax
+ * @author      Ilja Michajlow, <ilja.michajlow@mni.thm.de>
+ * @copyright   2015 TH Mittelhessen
+ * @license     GNU GPL v.2
+ * @link        www.mni.thm.de
+ */
+defined('_JEXEC') or die;
+jimport('joomla.application.component.view');
+/**
+ * Class loading persistent data into the view context
+ *
+ * @category    Joomla.Component.Site
+ * @package     thm_groups
+ * @subpackage  com_thm_groups.admin
+ * @link        www.mni.thm.de
+ */
+class THM_GroupsViewRoles_Ajax extends JViewLegacy
+{
+    /**
+     * loads model data into view context
+     *
+     * @param   string  $tpl  the name of the template to be used
+     *
+     * @return void
+     * 
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function display($tpl = null)
+    {
+        $model = $this->getModel();
+        $success = $model->getRolesOfGroup();
+        if($success)
+        {
+            echo json_encode($success);
+        }
+        else
+        {
+            echo 'ERROR';
+        }
+    }
+}
